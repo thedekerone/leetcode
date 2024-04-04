f4e4fd02e0b26e8c11b8b31f0e9d41a6d48691d8
Mauricio Fow <mauricio.fow@quanturi.com>
2024-04-04 17:01:18 +0300

    started zigzag

---
+++ zigzag/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void convert(char original[], char converted[], int numRows);
+
+int main(){
+    char initial[] ="PAYPALISHIRING";
+    char converted[1000];
+
+    printf("finished");
+}
+
+void convert(char original[], char converted[], int numRows){
+   char temp;
+
+}
